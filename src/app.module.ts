7b69f4e0ec164bf2170c23b9d380f1d6152e0445
Alexander Rodriguez <160079823+AXrodriguezQ@users.noreply.github.com>
2024-05-04 23:40:34 +0200

    persistence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './modules/users/users.module';
+import { LoginModule } from './modules/login/login.module';
+import { PersistenceModule } from './libs/persistence/persistence.module';
+import { ConfigModule } from '@nestjs/config';
+import dbConfig from './libs/persistence/db-config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+    envFilePath: '.env',
+    load: [dbConfig],
+    isGlobal: true,
+  }),
+  PersistenceModule,
+  UsersModule,
+  LoginModule
+],
 })
 export class AppModule {}
